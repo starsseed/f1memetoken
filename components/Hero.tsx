a5ffc9a71a6f0204f0bcf4338caa4fe9731bb4cb
starsseed <146839166+starsseed@users.noreply.github.com>
2024-03-10 22:38:08 +0800

    Update Hero.tsx
    adding anchor to view where is the reflection

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -25,7 +25,7 @@ export default function Hero(): ReactElement {
   return (
     <div>
       <h1 className="sr-only">
-        Hello I'm Dale Larroder, I'm a software developer, and I love building things for the web.
+        Hello Ioder, I'm a software developer, e building things for the web.
       </h1>
       <div className="relative z-10 flex h-[calc(100vh-81px)] items-center md:h-[calc(100vh-116px)]">
         <div className="mx-auto w-screen max-w-3xl px-4 sm:px-9 xl:max-w-5xl xl:px-0">
@@ -41,7 +41,7 @@ export default function Hero(): ReactElement {
                 href="/about"
                 className="underline-magical text-md w-max cursor-pointer sm:text-lg md:text-xl xl:text-2xl"
               >
-                Read more about me &rarr;
+                Read more abo9ut me &rarr;
               </Link>
             </div>
             <motion.div
