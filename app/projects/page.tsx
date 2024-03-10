c3e6628109ba140179b53bb34e7745b0d7cbd602
starsseed <146839166+starsseed@users.noreply.github.com>
2024-03-10 21:51:55 +0800

    Update page.tsx
    for real ??

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -11,10 +11,10 @@ export default function Page() {
     <MainLayout>
       <div className="space-y-2 pt-6 pb-8 md:space-y-5 ">
         <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
-          Projects
+          Roadmap
         </h1>
         <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-          Here are some of my selected projects worth sharing.
+          for real ? 
         </p>
       </div>
       <Projects />
