0a64a729d1dc91c9c3bc8e33c3f784cd3f7cc439
starsseed <146839166+starsseed@users.noreply.github.com>
2024-03-11 09:53:10 +0800

    Update layout.tsx
    favicon in png to the head.

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,9 +22,10 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/favicon.ico" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon.ico" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon.ico" />
+
+        <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
         <meta name="msapplication-TileColor" content="#000000" />
         <meta name="theme-color" content="#000000" />
         <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
