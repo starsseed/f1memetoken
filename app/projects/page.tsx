105e8eff97c18d183930b8249b4851d5f262bb57
starsseed <146839166+starsseed@users.noreply.github.com>
2024-03-11 19:18:48 +0800

    Update page.tsx
    adding humour

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -13,7 +13,7 @@ export default function Page() {
         <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
           Roadmap
         </h1>
-        <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">for real</p>
+        <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">for real? </p>
       </div>
       <Projects />
     </MainLayout>
