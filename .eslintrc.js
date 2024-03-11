144ae8284c196feea2f29500293b624027224ef0
starsseed <146839166+starsseed@users.noreply.github.com>
2024-03-11 09:28:23 +0800

    Update .eslintrc.js
    prettier off to avoid

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
     'next/core-web-vitals',
   ],
   rules: {
-    'prettier/prettier': ['error', { semi: true }],
+    'prettier/prettier': 'off',
     'react/react-in-jsx-scope': 'off',
     'jsx-a11y/anchor-is-valid': [
       'error',
