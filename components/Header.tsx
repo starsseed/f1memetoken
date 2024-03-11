abaad3897b0346b7e567b0d53257a5c9ea5b3656
starsseed <146839166+starsseed@users.noreply.github.com>
2024-03-11 10:34:22 +0800

    Update Header.tsx
    testing without @ for file url

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@
 
 import classNames from 'classnames';
 import headerNavLinks from 'content/headerNavLinks';
-import Logo from '@/content/logo.svg';
+import Logo from '/content/logo.svg';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import CommandPalette from './CommandPalette/CommandPalette';
