2e3cb029bab95ca998f8c1ee2d1141874d68adc6
starsseed <146839166+starsseed@users.noreply.github.com>
2024-03-11 18:27:31 +0800

    Update UsesLayout.tsx
    replace the previous what i use

--- components/layouts/MDX/UsesLayout.tsx
+++ components/layouts/MDX/UsesLayout.tsx
@@ -8,17 +8,17 @@ export default function UsesLayout({ children }: Props) {
     <div className="pt-8">
       <div className="mb-8 flex flex-col space-y-2 text-center">
         <h1 className="text-3xl font-extrabold leading-9 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
-          What I Use
+          How good is your F1 predictions?
         </h1>
         <span className="text-gray-700  dark:text-gray-300">
-          Inspired by{' '}
+          Suggested by{' '}
           <a
-            href="https://wesbos.com/uses"
+            href="https://chat.openai.com/"
             className="underline-magical"
             target="_blank"
             rel="noreferrer"
           >
-            Wes bos
+            chatGPT4
           </a>
         </span>
       </div>
