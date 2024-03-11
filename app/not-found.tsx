ba34e158b089cd0d8324ddfeec82096d99d06241
starsseed <146839166+starsseed@users.noreply.github.com>
2024-03-11 09:55:24 +0800

    Update not-found.tsx
    replace the author's name

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,8 +4,8 @@ import SectionContainer from '@/components/SectionContainer';
 // TODO: Comment out when https://github.com/vercel/next.js/issues/50566 is fixed.
 
 // export const metadata = {
-//   title: '404 - Dale Larroder',
-//   description: 'Not Found - Dale Larroder',
+//   title: '404 - missing block',
+//   description: 'Not Found - BOX BOX token',
 // };
 
 export default function FourZeroFour() {
