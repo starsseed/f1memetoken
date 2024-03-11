3cc7f4496e3e4ae06299c63e47cbdf1ed8edb048
starsseed <146839166+starsseed@users.noreply.github.com>
2024-03-11 18:11:13 +0800

    Update page.tsx
    the suggestion for every f1 fans in buying this meme token

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,8 +4,8 @@ import MainLayout from '@/layouts/MainLayout';
 import { allAuthors } from 'contentlayer/generated';
 
 export const metadata = {
-  title: 'About - Dale Larroder',
-  description: 'About me - Dale Larroder',
+  title: 'About - This Meme Token Game',
+  description: 'How would you play the game ? ',
 };
 
 export default function About() {
