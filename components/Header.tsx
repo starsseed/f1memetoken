874135d96b72ba0b3b5963d35124d921886dc22a
Hamdan Khan <116197286+Hamdan-Khan@users.noreply.github.com>
2024-03-29 18:37:39 +0500

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -56,7 +56,6 @@ export default function Header() {
             </div>
             <div className="flex items-center">
               <CommandPalette />
-             // <ThemeSwitch />
               <MobileNav />
             </div>
           </div>
